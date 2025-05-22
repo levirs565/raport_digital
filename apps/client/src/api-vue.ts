9f525b0e6a2a38a2830215fac6e3b1af6acf0a76
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2025-05-22 22:43:06 +0700

    fix(client): make reactivity work in trpc

--- apps/client/src/api-vue.ts
+++ apps/client/src/api-vue.ts
@@ -1,13 +1,47 @@
-import { inject } from "vue";
-import { trpc } from "./api";
-import { DataTag, dataTagErrorSymbol, dataTagSymbol, QueryClient, useQuery, UseQueryReturnType } from "@tanstack/vue-query";
+import { inject } from 'vue';
+import { trpc } from './api';
+import {
+  DataTag,
+  dataTagErrorSymbol,
+  dataTagSymbol,
+  QueryClient,
+  useQuery,
+  UseQueryOptions,
+  UseQueryReturnType,
+} from '@tanstack/vue-query';
 
-export const QUERY_CLIENT_KEY = Symbol()
-export const TRPC_KEY = Symbol()
-export const injectTrpc = () => inject<typeof trpc>(TRPC_KEY);
-export const injectQueryClient = () => inject<QueryClient>(QUERY_CLIENT_KEY)
+type MaybeRefDeep<T> = Extract<
+  Extract<
+    UseQueryOptions<any, any, any, any, [T]>,
+    { queryKey: any }
+  >['queryKey'],
+  any[]
+>[0];
+type MapQueryOptions<F> = F extends (
+  input: infer A,
+  ...rest: infer B
+) => infer R
+  ? (input: MaybeRefDeep<A>, ...rest: B) => R
+  : never;
+type MapToVue<T> = {
+  [K in keyof T]: K extends 'queryOptions'
+    ? MapQueryOptions<T[K]>
+    : T[K] extends object
+    ? MapToVue<T[K]>
+    : T[K];
+};
+type TRPCVue = MapToVue<typeof trpc>;
 
-export function useTrcpQuery<T extends { queryKey: DataTag<any, any, any> }>(options: T):
-  UseQueryReturnType<T["queryKey"][dataTagSymbol], T["queryKey"][dataTagErrorSymbol]> {
+export const QUERY_CLIENT_KEY = Symbol();
+export const TRPC_KEY = Symbol();
+export const injectTrpc = () => inject<TRPCVue>(TRPC_KEY);
+export const injectQueryClient = () => inject<QueryClient>(QUERY_CLIENT_KEY);
+
+export function useTrcpQuery<T extends { queryKey: DataTag<any, any, any> }>(
+  options: T
+): UseQueryReturnType<
+  T['queryKey'][dataTagSymbol],
+  T['queryKey'][dataTagErrorSymbol]
+> {
   return useQuery(options);
 }
