21dceee0997a8e460c146d270a6d3ac791bcdb32
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2025-06-10 11:32:09 +0700

    feat(client): add validationt to periode

--- apps/client/src/pages/operator/periode/AddPeriode.vue
+++ apps/client/src/pages/operator/periode/AddPeriode.vue
@@ -4,6 +4,9 @@ import { computed, ref, watchEffect } from 'vue';
 import { injectTrpc, useTrcpQuery } from '../../../api-vue';
 import { useMutation, useQueryClient } from '@tanstack/vue-query';
 import { useRouter } from 'vue-router';
+import { SubmitEventPromise } from 'vuetify';
+import { useRules } from 'vuetify/labs/rules';
+import { formatError } from '../../../api';
 
 const { id } = defineProps({
   id: String
@@ -35,8 +38,8 @@ const selectedSemester = ref<SemesterType>();
 
 const trpc = injectTrpc();
 const queryClient = useQueryClient();
-const { mutateAsync: addAsync } = useMutation(trpc!.operator.periodeAjar.add.mutationOptions())
-const { mutateAsync: updateAsync } = useMutation(trpc!.operator.periodeAjar.update.mutationOptions());
+const { mutateAsync: addAsync, error, isPending, reset } = useMutation(trpc!.operator.periodeAjar.add.mutationOptions())
+const { mutateAsync: updateAsync, error: updateError, isPending: updateIsPending, reset: updateReset } = useMutation(trpc!.operator.periodeAjar.update.mutationOptions());
 
 const { data } = useTrcpQuery(trpc!.operator.periodeAjar.get.queryOptions({
   id: id!
@@ -53,7 +56,13 @@ watchEffect(() => {
 
 const router = useRouter();
 
-function onSubmit() {
+async function onSubmit(event: SubmitEventPromise) {
+  if (!(await event).valid) {
+    reset();
+    updateReset();
+    return;
+  }
+
   if (!selectedTahunAjar.value || !selectedSemester.value) return;
   const update = () => {
     queryClient.invalidateQueries({
@@ -84,17 +93,24 @@ function onSubmit() {
       emit('close')
     })
 }
+
+const rules = useRules();
 </script>
 <template>
-  <v-card>
-    <v-toolbar color="surface">
-      <v-btn icon="mdi-close" @click="$emit('close')"></v-btn>
-      <v-app-bar-title>{{ id ? "Ubah" : "Tambah" }} Periode Ajar</v-app-bar-title>
-    </v-toolbar>
-    <v-form class="px-4 py-2">
-      <v-select label="Tahun Ajaran" v-model="selectedTahunAjar" :items="tahunAjarSelectItems" />
-      <v-select label="Semester" v-model="selectedSemester" :items="semesterSelectItems" />
-      <v-btn @click="onSubmit">{{ id ? "Ubah" : "Tambah" }}</v-btn>
-    </v-form>
-  </v-card>
+  <v-form @submit.prevent="onSubmit">
+    <v-card>
+      <v-toolbar color="surface">
+        <v-btn icon="mdi-close" @click="$emit('close')"></v-btn>
+        <v-app-bar-title>{{ id ? "Ubah" : "Tambah" }} Periode Ajar</v-app-bar-title>
+      </v-toolbar>
+      <div class="px-4 py-2">
+        <v-select :rules="[rules!.required!()]" label="Tahun Ajaran" v-model="selectedTahunAjar"
+          :items="tahunAjarSelectItems" />
+        <v-select :rules="[rules!.required!()]" label="Semester" v-model="selectedSemester"
+          :items="semesterSelectItems" />
+        <v-card-text class="text-error text-center pa-0 my-2" v-if="id ? updateError : error">{{ formatError(id ? updateError : error) }}</v-card-text>
+        <v-btn class="my-2" type="submit" :loading="id ? updateIsPending : isPending">{{ id ? "Ubah" : "Tambah" }}</v-btn>
+      </div>
+    </v-card>
+  </v-form>
 </template>
