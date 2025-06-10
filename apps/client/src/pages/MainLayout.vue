9280fb1654f63c10faf1ac0e22096e13b5728e9e
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2025-06-10 13:37:18 +0700

    feat(client): add icon to navigation draer

--- apps/client/src/pages/MainLayout.vue
+++ apps/client/src/pages/MainLayout.vue
@@ -94,42 +94,49 @@ const { data: kepalaSekolahKelasData } = useTrcpQuery(trpc!.kepalaSekolah.getAll
 
 </script>
 <template>
-  <v-navigation-drawer v-model="drawer">
-    <v-list-item :title="accountTitle" :subtitle="accountSubtitle" />
-    <v-list-item title="Akun" to="/akun" />
+  <v-navigation-drawer v-model="drawer"
+    style="border-bottom-right-radius: 16px; border-top-right-radius: 16px overflow: hidden;">
+    <div class="pa-4 d-flex flex-column align-center text-center">
+      <v-icon :size="48" class="mb-2">mdi-account-circle-outline</v-icon>
+      <p class="text-h6">{{ accountTitle }}</p>
+      <p class="text-body-2">{{ accountSubtitle }}</p>
+    </div>
+    <v-list-item prepend-icon="mdi-account-circle-outline" title="Akun" to="/akun" />
     <template v-if="data?.type == 'OPERATOR'">
       <v-divider />
-      <v-list-item title="Dashboard" to="/" exact></v-list-item>
-      <v-list-item title="Verifikasi Akun Guru" to="/operator/akun-guru"></v-list-item>
-      <v-list-item title="Siswa" to="/operator/siswa"></v-list-item>
-      <v-list-item title="Periode Ajar" to="/operator/periode" />
+      <v-list-item prepend-icon="mdi-home-outline" title="Dashboard" to="/" exact></v-list-item>
+      <v-list-item prepend-icon="mdi-folder-outline" title="Verifikasi Akun Guru"
+        to="/operator/akun-guru"></v-list-item>
+      <v-list-item prepend-icon="mdi-folder-outline" title="Siswa" to="/operator/siswa"></v-list-item>
+      <v-list-item prepend-icon="mdi-folder-outline" title="Periode Ajar" to="/operator/periode" />
       <v-divider />
     </template>
     <v-select v-model="periodeStore.selectedPeriode" :items="periodeSelectItems" item-value="id_periode_ajar"
       item-title="title" variant="underlined" class="mx-4" />
     <template v-if="data?.type == 'OPERATOR'">
-      <v-list-item title="Mata Pelajaran" to="/operator/mata-pelajaran" />
-      <v-list-item title="Ekstrakurikuler" to="/operator/ekstrakurikuler" />
-      <v-list-item title="Kelas" to="/operator/kelas" />
+      <v-list-item prepend-icon="mdi-folder-outline" title="Mata Pelajaran" to="/operator/mata-pelajaran" />
+      <v-list-item prepend-icon="mdi-folder-outline" title="Ekstrakurikuler" to="/operator/ekstrakurikuler" />
+      <v-list-item prepend-icon="mdi-folder-outline" title="Kelas" to="/operator/kelas" />
     </template>
     <template v-if="data?.type == 'GURU'">
       <v-divider />
       <template v-if="waliKelasData?.length">
         <v-list-subheader class="px-4">Wali Kelas</v-list-subheader>
-        <v-list-item v-for="item in waliKelasData" :key="item.id_kelas" :to="`/guru/wali-kelas/${item.id_kelas}`">
+        <v-list-item prepend-icon="mdi-folder-outline" v-for="item in waliKelasData" :key="item.id_kelas"
+          :to="`/guru/wali-kelas/${item.id_kelas}`">
           <v-list-item-title>Kelas {{ item.kelas }}-{{ item.kode_ruang_kelas }}</v-list-item-title>
         </v-list-item>
       </template>
       <template v-if="ekstrakurikulerData?.length">
         <v-list-subheader class="px-4">Ekstrakurikuler</v-list-subheader>
-        <v-list-item v-for="item in ekstrakurikulerData" :key="item.id_esktrakurikuler"
-          :to="`/guru/ekstrakurikuler/${item.id_esktrakurikuler}`">
+        <v-list-item prepend-icon="mdi-folder-outline" v-for="item in ekstrakurikulerData"
+          :key="item.id_esktrakurikuler" :to="`/guru/ekstrakurikuler/${item.id_esktrakurikuler}`">
           <v-list-item-title>{{ item.nama }}</v-list-item-title>
         </v-list-item>
       </template>
       <template v-if="mataPelajaranData?.length">
         <v-list-subheader class="px-4">Mata Pelajaran</v-list-subheader>
-        <v-list-item v-for="item in mataPelajaranData"
+        <v-list-item prepend-icon="mdi-folder-outline" v-for="item in mataPelajaranData"
           :key="`${item.kelas.id_kelas}-${item.mata_pelajaran.id_mata_pelajaran}`"
           :to="`/guru/mata-pelajaran/${item.kelas.id_kelas}/${item.mata_pelajaran.id_mata_pelajaran}`">
           <v-list-item-title>Kelas {{ item.kelas.kelas }}-{{ item.kelas.kode_ruang_kelas }} - {{
@@ -138,7 +145,8 @@ const { data: kepalaSekolahKelasData } = useTrcpQuery(trpc!.kepalaSekolah.getAll
       </template>
       <template v-if="p5Data?.length">
         <v-list-subheader class="px-4">Koordinator P5</v-list-subheader>
-        <v-list-item v-for="item in p5Data" :key="item.id_kelas" :to="`/guru/p5/${item.id_kelas}`">
+        <v-list-item prepend-icon="mdi-folder-outline" v-for="item in p5Data" :key="item.id_kelas"
+          :to="`/guru/p5/${item.id_kelas}`">
           <v-list-item-title>Kelas {{ item.kelas }}-{{ item.kode_ruang_kelas }}</v-list-item-title>
         </v-list-item>
       </template>
@@ -146,13 +154,13 @@ const { data: kepalaSekolahKelasData } = useTrcpQuery(trpc!.kepalaSekolah.getAll
     <template v-if="data?.type == 'KEPALA_SEKOLAH'">
       <v-divider />
       <v-list-subheader class="px-4">Verifikasi Raport Siswa</v-list-subheader>
-      <v-list-item v-for="item in kepalaSekolahKelasData" :key="item.id_kelas"
+      <v-list-item prepend-icon="mdi-folder-outline" v-for="item in kepalaSekolahKelasData" :key="item.id_kelas"
         :to="`/kepala-sekolah/kelas/${item.id_kelas}`">
         <v-list-item-title>Kelas {{ item.kelas }}-{{ item.kode_ruang_kelas }}</v-list-item-title>
       </v-list-item>
     </template>
     <v-divider />
-    <v-list-item title="Logout" @click="onLogout" />
+    <v-list-item prepend-icon="mdi-logout" title="Logout" @click="onLogout" />
   </v-navigation-drawer>
   <router-view />
 </template>
